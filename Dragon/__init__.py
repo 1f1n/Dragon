539ece921788cebe0eefd640cc418c391c080f8a
fin <119663332+1f1n@users.noreply.github.com>
2024-10-09 14:26:16 +0100

    Update __init__.py

--- Dragon/__init__.py
+++ Dragon/__init__.py
@@ -1,6 +1,7 @@
 from Dragon.utils import *
 from Dragon.scan import ScanAllTx
 from Dragon.traders import TopTraders
+from Dragon.holders import TopHolders
 from Dragon.bundle import BundleFinder
 from Dragon.wallet import BulkWalletChecker
 from Dragon.timestamp import TimestampTransactions
