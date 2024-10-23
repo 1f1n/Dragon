bb5a146e1cbd890621e05f0f987b6e12b73198e8
fin <119663332+1f1n@users.noreply.github.com>
2024-10-23 22:14:11 +0100

    Update traders.py

--- Dragon/traders.py
+++ Dragon/traders.py
@@ -1,10 +1,11 @@
 import json
+import time
+import random
 import tls_client
 import cloudscraper
 from fake_useragent import UserAgent
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from collections import defaultdict
-import time
 
 ua = UserAgent(os='linux', browsers=['firefox'])
 
@@ -19,6 +20,35 @@ class TopTraders:
         self.addressFrequency = defaultdict(int)
         self.totalTraders = 0
         self.proxyPosition = 0
+    
+    def randomise(self):
+        self.identifier = random.choice([browser for browser in tls_client.settings.ClientIdentifiers.__args__ if browser.startswith(('chrome', 'safari', 'firefox', 'opera'))])
+        self.sendRequest = tls_client.Session(random_tls_extension_order=True, client_identifier=self.identifier)
+
+        parts = self.identifier.split('_')
+        identifier, version, *rest = parts
+        other = rest[0] if rest else None
+        
+        os = 'windows'
+        if identifier == 'opera':
+            identifier = 'chrome'
+        elif version == 'ios':
+            os = 'ios'
+        else:
+            os = 'windows'
+
+        self.user_agent = UserAgent(browsers=[identifier], os=[os]).random
+
+        self.headers = {
+            'Host': 'gmgn.ai',
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+            'dnt': '1',
+            'priority': 'u=1, i',
+            'referer': 'https://gmgn.ai/?chain=sol',
+            'user-agent': self.user_agent
+        }
+
 
     def loadProxies(self):
         with open("Dragon/data/Proxies/proxies.txt", 'r') as file:
@@ -62,25 +92,24 @@ class TopTraders:
     def fetchTopTraders(self, contractAddress: str, useProxies):
         url = f"https://gmgn.ai/defi/quotation/v1/tokens/top_traders/sol/{contractAddress}?orderby=profit&direction=desc"
         retries = 3
-        headers = {
-            "User-Agent": ua.random
-        }
-        
+
         for attempt in range(retries):
             try:
+                self.randomise()
                 proxy = self.getNextProxy() if useProxies else None
                 self.configureProxy(proxy)
-                response = self.sendRequest.get(url, headers=headers)
+                response = self.sendRequest.get(url, headers=self.headers, allow_redirects=True)
                 data = response.json().get('data', None)
                 if data:
                     return data
             except Exception as e:
                 print(f"[🐲] Error fetching data on attempt, trying backup... {e}")
             finally:
+                self.randomise()
                 try:
                     proxy = self.getNextProxy() if useProxies else None
                     proxies = {'http': proxy, 'https': proxy} if proxy else None
-                    response = self.cloudScraper.get(url, headers=headers, proxies=proxies)
+                    response = self.cloudScraper.get(url, headers=self.randomise(), proxies=proxies)
                     data = response.json().get('data', None)
                     if data:
                         return data
